83ef6f4e6cbae9fc9b3c8bc897c1ebfd54eb96af
shuyang1997 <1723205288@qq.com>
2021-10-24 21:13:37 +0800

    初始化项目

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+/*
+ * @Author: shuyang
+ * @Date: 2021-10-24 20:42:16
+ * @LastEditTime: 2021-10-24 21:08:45
+ * @FilePath: \my_blog\pages\_app.tsx
+ */
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import 'antd/dist/antd.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
